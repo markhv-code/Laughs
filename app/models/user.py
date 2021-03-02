22d11992cd1558fc19eb91433afb6e13dd9c7f30
Mark Valdez <mhvaldez209@gmail.com>
2021-03-02 13:28:46 -1000

    ready up for seeding

--- app/models/user.py
+++ app/models/user.py
@@ -1,22 +1,25 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    imageURL = db.Column(db.String(2083), nullable=True)
+    join_date = db.Column(db.Datetime, index=True, default=datetime.now)
 
-    messages_sent = db.relationship(
-        "Message", foreign_keys="Message.senderId", back_populates="sender"
-    )
-    messages_received = db.relationship(
-        "Message", foreign_keys="Message.receiverId", back_populates="receiver"
-    )
+    # messages_sent = db.relationship(
+    #     "Message", foreign_keys="Message.senderId", back_populates="sender"
+    # )
+    # messages_received = db.relationship(
+    #     "Message",foreign_keys="Message.receiverId",back_populates="receiver"
+    # )
 
     @property
     def password(self):
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,6 +1,7 @@
 from werkzeug.security import generate_password_hash
 from app.models import db, User
 
+
 # Adds a demo user
 def seed_users():
 
@@ -9,15 +10,12 @@ def seed_users():
             username="Demo",
             email="demo@aa.io",
             password="password",
-            city="Salt Lake",
-            stateAbbr="UT",
+            imageURL="https://assets-global.website-files.com/5dcc7f8c449e597ed83356b8/5dcc8a5e5473766654e080bb_5d93d0a059ce656959dab196_logo-full-black-2000-p-500.png"
         ),
         User(
             username="Demo2",
             email="demo2@aa.io",
             password="password",
-            city="Provo",
-            stateAbbr="UT",
         ),
     ]
 
