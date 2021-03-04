b1de1cd3529273292087b4497a69bd0eb2b3f449
Mark Valdez <mhvaldez209@gmail.com>
2021-03-04 10:56:03 -1000

    made joke form

--- app/api/joke_routes.py
+++ app/api/joke_routes.py
@@ -3,6 +3,40 @@ from flask_login import login_required
 from werkzeug.utils import secure_filename
 
 from app.helpers import upload_file_to_s3
-from app.models import Pet, db
-from app.forms import CreatePetForm
+from app.models import Joke, db
+from app.forms import CreateJokeForm
 from app.api.auth_routes import validation_errors_to_error_messages
+
+joke_routes = Blueprint("jokes", __name__)
+
+
+@joke_routes.route("")
+def get_jokes():
+    """
+    Get all jokes
+    """
+    jokes = Joke.query.all()
+    return {"jokes": [joke.to_dict() for joke in jokes]}
+
+
+@joke_routes.route("", methods=["POST"])
+@login_required
+def create_joke():
+    """
+    Create new joke
+    """
+    form = CreateJokeForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+
+    image_error = []
+    image = request.files.get("image", None)
+
+    if image is not None:
+        image.filename = secure_filename(image.filename)
+        pattern = re.compile(
+            ".*(apng|avif|jpe?g|png|svg|webp)$", re.IGNORECASE)
+        is_image = bool(pattern.match(image.mimetype))
+        if not is_image:
+            image_error.append(
+                "Upload must be an image (apng, avif, jpeg/jpg, png, svg, webp)."
+            )
---
+++ app/forms/joke_form.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired, ValidationError, Length, Optional
+from app.models import Joke
+
+
+class CreateJokeForm(FlaskForm):
+    userId = IntegerField(
+        "userId", validators=[DataRequired(message="Must associate with a user")]
+    )
+    joke = StringField(
+        'joke', validators=[
+            DataRequired(message="A joke is required"),
+            Length(min=1, max=800, message="Joke must be less than 800 characters"), ]
+    )
+    jokeType = StringField(
+        "jokeType",
+        validators=[
+            DataRequired(message="Must specify joke type"),
+            Length(min=1, max=50,
+                   message="Joke type must be less than 50 characters"),
+        ],
+    )
+    imageURL = StringField(
+        "imageURL",
+        validators=[Optional],
+    )
+    threadId = IntegerField("threadId", validators=[DataRequired()])
