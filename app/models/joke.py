d8e040679f9df592313959a2cda1d437a1661ba1
Mark Valdez <mhvaldez209@gmail.com>
2021-03-04 09:56:37 -1000

    finish joke seeds

--- app/models/joke.py
+++ app/models/joke.py
@@ -8,7 +8,7 @@ class Joke(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userId = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     joke = db.Column(db.String(800), nullable=False)
-    imageURL = db.Column(db.String(2083), nullable=False)
+    imageURL = db.Column(db.String(2083), nullable=True)
     jokeType = db.Column(db.String(50), nullable=False)
     threadId = db.Column(db.Integer, db.ForeignKey("threads.id"), nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
--- app/seeds/jokes.py
+++ app/seeds/jokes.py
@@ -7,13 +7,15 @@ def seed_jokes():
         Joke(
             userId=1,
             joke="I ate a clock yesterday, it was very time-consuming.",
-            imageURL="https://assets-global.website-files.com/5dcc7f8c449e597ed83356b8/5dcc8a5e5473766654e080bb_5d93d0a059ce656959dab196_logo-full-black-2000-p-500.png"
-            jokeType="Pun",
+            imageURL="https://assets-global.website-files.com/5dcc7f8c449e597ed83356b8/5dcc8a5e5473766654e080bb_5d93d0a059ce656959dab196_logo-full-black-2000-p-500.png",
+            jokeType="Misc",
+            threadId=1,
         ),
         Joke(
-            username="Demo2",
-            email="demo2@aa.io",
-            password="password",
+            userId=2,
+            joke="A perfectionist walked into a bar...apparently, the bar wasn’t set high enough.",
+            jokeType="Any",
+            threadId=2,
         ),
     ]
 
