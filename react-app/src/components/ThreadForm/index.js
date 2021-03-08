5989daef1e8a88a4090accc4c09752835e290b0a
Mark Valdez <mhvaldez209@gmail.com>
2021-03-08 13:56:27 -1000

    break time

--- react-app/src/components/ThreadForm/index.js
+++ react-app/src/components/ThreadForm/index.js
@@ -1,29 +1,46 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { createThread } from "../../store/threads";
 
+export default function ThreadForm({ id }) {
+  const currentUser = useSelector((state) => state.session.user);
+  const dispatch = useDispatch();
+  const currentJoke = useSelector((state) => state.jokes);
 
-export default function ThreadForm(){
-    const currentUser = useSelector((state) => state.session.user);
-    const dispatch = useDispatch();
-    const currentJoke = useSelector((state) => state.jokes)
+  const [comment, setComment] = useState("");
+  const [errors, setErrors] = useState([]);
 
-    const [comment, setComment] = useState('');
-    const [errors, setErrors] = useState([]);
+  const createThreadPost = async (e) => {
+    e.preventDefault();
+    setErrors([]);
 
-    const createThreadPost = async (e) => {
-        e.preventDefault();
-        setErrors([]);
-        
-        const thread = {
-            userId: currentUser.id,
-            // jokeId: ,
-            comment: comment,
-        };
+    const thread = {
+      userId: currentUser.id,
+      jokeId: id,
+      comment: comment,
+    };
 
-        const sendThread = await dispatch((thread));
+    const sendThread = await dispatch(createThread(thread));
+    if (sendThread) {
+      setComment("");
+    } else {
+      setErrors(sendThread.errors);
     }
+  };
 
-    return (
-        <div>Thread</div>
-    )
+  return (
+    <>
+      <form>
+        <input
+          className="w-full rounded-lg border-4"
+          name="comment"
+          value={comment}
+          onChange={(e) => setComment(e.target.balue)}
+          placeholder="Leave a comment!"
+          required
+        />
+        <button type="submit">Post</button>
+      </form>
+    </>
+  );
 }
