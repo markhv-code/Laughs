ddcf14dd237596c484f1b1ebf3097855c5cac3e7
Mark Valdez <mhvaldez209@gmail.com>
2021-03-08 12:33:53 -1000

    joke feed initial stage complete

--- react-app/src/components/JokeFeed/index.js
+++ react-app/src/components/JokeFeed/index.js
@@ -9,12 +9,13 @@ export default function JokeFeed() {
     return (
         <div className="col-start-3 col-end-6 row-start-5 row-end-7 w-full h-full">
             {allJokes.map((post) => {
-                const { id, imageURL, joke } = post;
+                const { id, imageURL, joke, jokeType } = post;
                 const myDate = new Date(post.users.timestamp)
                 return (
                     <div key={id} className="rounded-lg border-4 border-light-blue-500 border-opacity-50 p-1 m-2">
                         {/* <img src={imageURL} alt=''/> */}
                             <h3>{post.users.username}</h3>
+                            <h3>Joke Type: {post.jokeType}</h3>
                             <h3>{myDate.toLocaleString()}</h3>
                             <h3 className="text-lg">{joke}</h3>
                     </div>
