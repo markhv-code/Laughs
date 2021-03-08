5e65712a62f293b4bc32a6a35e935cf9f5786ec8
Mark Valdez <mhvaldez209@gmail.com>
2021-03-08 06:55:05 -1000

    getJokes from redux store

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-// import { BrowserRouter, Switch } from 'react-router-dom';
 
 // components
 
-import NavBar from './components/NavBar/index.js'
+// import NavBar from './components/NavBar/index.js'
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/Users/UsersList';
 import User from './components/Users/User';
--- react-app/src/components/JokeFeed/index.js
+++ react-app/src/components/JokeFeed/index.js
@@ -1,5 +1,11 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { getJokes } from '../../store/jokes';
 
 export default function JokeFeed() {
+    const dispatch = useDispatch();
+    dispatch(getJokes);
+    const allJokes = useSelector(state => state.jokes)
+
     return (
         <div>
             <h1>Hello World</h1>
