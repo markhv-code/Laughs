41d0cb4fb8bdcd5f9be6df81dd0e57fc0eb6ec82
Mark Valdez <mhvaldez209@gmail.com>
2021-03-08 13:26:08 -1000

    setup thread Reducer

--- react-app/src/store/threads.js
+++ react-app/src/store/threads.js
@@ -1,14 +1,44 @@
 const ADD_THREAD = '/jokes/ADD_THREAD';
 
-const addthread = thread => ({
+const addThread = thread => ({
     type: ADD_THREAD,
     thread,
 })
 
 export const createThread = (newThread) => async (dispatch) => {
-    const res = await fetch('/api/threads');
-    const json = await res.json();
-    if(res.ok){
-        dispatch(addthread(json))
-    } 
-}
+    const { userId, jokeId, comment } = newThread;
+
+    const formData = new FormData();
+    formData.append('userId', userId);
+    formData.append('jokeId', jokeId);
+    formData.append('comment', comment);
+    
+    const res = await fetch('/api/threads', {
+      method: 'POST',
+      body: formData,
+    });
+    const addedThread = await res.json();
+    if (!addedThread.errors) {
+      dispatch(addThread(addedThread));
+      return addedThread;
+    } else {
+      const errors = addedThread;
+      return errors;
+    };
+};
+
+const initState = {};
+
+const threadReducer = (state = initState, action) => {
+    const newState = {...state};
+
+    switch(action.type){
+        case ADD_THREAD:
+            newState[action.thread.id] = action.thread;
+            return newState;
+        default:
+            return newState;
+    };
+};
+
+export default threadReducer;
