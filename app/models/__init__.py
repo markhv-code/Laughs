1fe8f89717ba8a3ead7702f1002d89486647c31c
Mark Valdez <mhvaldez209@gmail.com>
2021-03-10 07:49:04 -1000

    fix import

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,6 @@ from .db import db
 from .user import User
 from .thread import Thread
 from .joke import Joke
+from .message import Message
 # from .pet import Pet, best_friends
-# from .message import Message
+
