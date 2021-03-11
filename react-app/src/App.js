69783694a791da4072c4fb48532f389736895547
Mark Valdez <mhvaldez209@gmail.com>
2021-03-11 06:37:43 -1000

    first attempt at fixing auth bug

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 // components
 
 // import NavBar from './components/NavBar/index.js'
@@ -12,27 +12,37 @@ import HomeFeed from "./components/HomeFeed/index";
 import Messages from './components/Messages';
 
 
-import { useModalAndAuthContext } from './context/ModalAndAuth';
+// import { useModalAndAuthContext } from './context/ModalAndAuth';
 import { getJokes } from './store/jokes';
 import { getThreads } from './store/threads';
+import { getMessages } from './store/messages';
+import { setUser } from './store/session';
+import { getUsers } from './store/users';
 import { authenticate } from './services/auth';
 
 function App() {
-  const { authenticated, setAuthenticated } = useModalAndAuthContext();
+  // const { authenticated, setAuthenticated } = useModalAndAuthContext();
   const dispatch = useDispatch();
   const [loaded, setLoaded] = useState(false);
+  const sessionUser = useSelector((state) => state.session.user);
+
 
   useEffect(() => {
     dispatch(getJokes());
     dispatch(getThreads());
+    dispatch(getMessages());
+    dispatch(getUsers());
+
     (async () => {
       const user = await authenticate();
       if (!user.errors) {
-        setAuthenticated(true);
+        // setAuthenticated(true);
+        dispatch(setUser(user));
+
       }
       setLoaded(true);
     })();
-  }, [setAuthenticated, dispatch]);
+  }, [dispatch]);
 
   if (!loaded) {
     return 'loading...';
@@ -48,24 +58,24 @@ function App() {
         <ProtectedRoute
           path='/users'
           exact={true}
-          authenticated={authenticated}
+          authenticated={!!sessionUser}
         >
           <UsersList />
         </ProtectedRoute>
         <ProtectedRoute
           path='/users/:userId'
           exact={true}
-          authenticated={authenticated}
+          authenticated={!!sessionUser}
         >
           <User />
         </ProtectedRoute>
-        <ProtectedRoute path='/home' exact={true} authenticated={authenticated}>
+        <ProtectedRoute path='/home' exact={true} authenticated={!!sessionUser}>
           <HomeFeed />
         </ProtectedRoute>
         <ProtectedRoute
           path='/messages'
           exact={true}
-          authenticated={authenticated}
+          authenticated={!!sessionUser}
           >
           <Messages />
         </ProtectedRoute>
