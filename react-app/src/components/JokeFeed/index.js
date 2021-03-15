290a489edfdbc1ee1308f5949fe2a6ca1f12e2bc
Mark Valdez <mhvaldez209@gmail.com>
2021-03-14 17:10:12 -1000

    add scroll component

--- react-app/src/components/JokeFeed/index.js
+++ react-app/src/components/JokeFeed/index.js
@@ -1,37 +1,49 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { getJokes } from '../../store/jokes';
 import ThreadForm from '../ThreadForm';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default function JokeFeed() {
     const dispatch = useDispatch();
     dispatch(getJokes);
-    const allJokes = useSelector(state => Object.values(state.jokes));
+    const allJokes = useSelector(state => Object.values(state.jokes({ length: 20 })));
     const jokeComments = useSelector(state => Object.values(state.threads));
     // const jokeComments = comments.length ? comments : [];
     // console.log("----------jokeComments-------", jokeComments);
+    
 
     return (
         <div className="col-start-3 col-end-6 row-start-5 row-end-7 w-full h-full" >
-            {allJokes.map((post) => {
-                const { id, imageURL, joke, jokeType } = post;
-                const myDate = new Date(post.users.timestamp);
-                const filteredComments = jokeComments.filter(joke => (joke.jokeId === id));
-                // console.log("-----------filtered-----", filteredComments);
-                return (
-                    <div key={id} className="rounded-lg border-4 border-light-blue-500 border-opacity-50 p-1 m-2">
-                            <h3>{post.users.username}</h3>
-                            <h3>Joke Type: {jokeType}</h3>
-                            <h3>{myDate.toLocaleString()}</h3>
-                            <h3 className="text-lg">{joke}</h3>
-                            {filteredComments && filteredComments.map(comment => (
-                                <div key={comment.id} className="text-sm text-center">
-                                    {comment.comment}
-                                </div>
-                            ))}
-                            <ThreadForm id={id} />
-                    </div>
-                )
-            })}
+            <InfiniteScroll
+                dataLength={allJokes.length}
+                loader={<h4>Loading...</h4>}
+                endMessage={
+                    <p style={{ textAlign: 'center' }}>
+                        <b>All caught up!</b>
+                    </p>
+                }
+            >  
+                {allJokes.map((post) => {
+                    const { id, imageURL, joke, jokeType } = post;
+                    const myDate = new Date(post.users.timestamp);
+                    const filteredComments = jokeComments.filter(joke => (joke.jokeId === id));
+                    // console.log("-----------filtered-----", filteredComments);
+                    return (
+                        <div key={id} className="rounded-lg border-4 border-light-blue-500 border-opacity-50 p-1 m-2">
+                                <h3>{post.users.username}</h3>
+                                <h3>Joke Type: {jokeType}</h3>
+                                <h3>{myDate.toLocaleString()}</h3>
+                                <h3 className="text-lg">{joke}</h3>
+                                {filteredComments && filteredComments.map(comment => (
+                                    <div key={comment.id} className="text-sm text-center">
+                                        {comment.comment}
+                                    </div>
+                                ))}
+                                <ThreadForm id={id} />
+                        </div>
+                    )
+                })}
+            </InfiniteScroll> 
         </div>
     )
 }
