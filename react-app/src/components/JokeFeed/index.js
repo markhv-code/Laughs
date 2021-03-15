84421d6aaa84c6e905c4a7e3434b2235a4c0b510
Mark Valdez <mhvaldez209@gmail.com>
2021-03-14 17:28:37 -1000

    can't get to work properly

--- react-app/src/components/JokeFeed/index.js
+++ react-app/src/components/JokeFeed/index.js
@@ -6,16 +6,29 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 export default function JokeFeed() {
     const dispatch = useDispatch();
     dispatch(getJokes);
-    const allJokes = useSelector(state => Object.values(state.jokes({ length: 20 })));
+    const allJokes = useSelector(state => Object.values(state.jokes));
     const jokeComments = useSelector(state => Object.values(state.threads));
     // const jokeComments = comments.length ? comments : [];
     // console.log("----------jokeComments-------", jokeComments);
-    
+    const fetchMoreData = () => {
+    if (allJokes.length >= 50) {
+      this.setState({ hasMore: false });
+      return;
+    }
+    // a fake async api call like which sends
+    // 20 more records in .5 secs
+    setTimeout(() => {
+      this.setState({
+        items: this.state.items.concat(Array.from({ length: 20 }))
+      });
+    }, 500);
+  };
 
     return (
         <div className="col-start-3 col-end-6 row-start-5 row-end-7 w-full h-full" >
             <InfiniteScroll
                 dataLength={allJokes.length}
+                next={fetchMoreData}
                 loader={<h4>Loading...</h4>}
                 endMessage={
                     <p style={{ textAlign: 'center' }}>
