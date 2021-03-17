dc8a3476e1f7e29ab6b9613b0e7e422a546f6f37
Mark Valdez <mhvaldez209@gmail.com>
2021-03-17 11:00:55 -1000

    implment basic navbar

--- react-app/src/components/SplashPage/splashPage.js
+++ react-app/src/components/SplashPage/splashPage.js
@@ -1,29 +1,31 @@
 import SignUpFormModal from '../auth/SignUpFormModal';
 import LoginFormModal from '../auth/LoginFormModal';
+import { NavLink } from 'react-router-dom';
 import { useModalAndAuthContext } from '../../context/ModalAndAuth';
 
 export default function SplashPage() {
     return (
         <>
-            <div>
-                <img className="object-contain" alt="Laughs App logo" src='https://pairyopet.s3-us-west-1.amazonaws.com/pyp-logo-cropped.png'></img>
-            </div>
-            <div>
-                <div className="absolute top-0 right-0 p-2 m-2">
-                    <LoginFormModal/>
+            <nav className='bg-red-joker h-20 grid grid-flow-col grid-cols-10 grid-rows-1'>
+                <div className="h-1/2">
+                    <img className='' alt="Laughs App logo" src='https://pairyopet.s3-us-west-1.amazonaws.com/pyp-logo-cropped.png'></img>
+                    Laughs
                 </div>
-                <div className="absolute top-5 right-0 p-2 m-2">
-                    <SignUpFormModal/>
+                <div className="col-start-9 col-end-10">
+                        <LoginFormModal/>
                 </div>
-            </div>
+                <div className="col-start-10 col-end-10">
+                        <SignUpFormModal/>
+                </div>
+            </nav>
             <div className="grid grid-flow-col grid-cols-4 grid-rows-4">
-                <h1 className="text-4xl col-start-1 col-end-2 row-start-1 row-end-2 p-1 m-1">Sharing jokes around the world</h1>
-                <h2 className="text-lg col-start-1 col-end-2 row-start-2 row-end-3 p-1 m-1">Use Laughs and all its features for free, forever!</h2>
-                <div className="col-start-2 col-end-5 row-start-1 row-end-4">
-                    <img className="rounded-lg md:w-1/2" alt="Laughing Man" src='https://pairyopet.s3-us-west-1.amazonaws.com/laughing-man-holding-pen-in-meeting.jpg'></img>
-                </div>
+                    <h1 className="text-4xl col-start-1 col-end-2 row-start-1 row-end-2 p-1 m-1">Sharing jokes around the world</h1>
+                    <h2 className="text-lg col-start-1 col-end-2 row-start-2 row-end-3 p-1 m-1">Use Laughs and all its features for free, forever!</h2>
+                    <div className="col-start-2 col-end-5 row-start-1 row-end-4">
+                        <img className="rounded-lg md:w-1/2" alt="Laughing Man" src='https://pairyopet.s3-us-west-1.amazonaws.com/laughing-man-holding-pen-in-meeting.jpg'></img>
+                    </div>
             </div>
-            
+ 
         </>
     )
 }
