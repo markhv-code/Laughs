c8b58fdc8e4999712a76d48a9dd34b2d5071f6b3
Mark Valdez <mhvaldez209@gmail.com>
2021-03-16 21:19:27 -1000

    move images to public folder

--- react-app/src/components/SplashPage/splashPage.js
+++ react-app/src/components/SplashPage/splashPage.js
@@ -6,7 +6,7 @@ export default function SplashPage() {
     return (
         <>
             <div>
-                <img className="object-contain" alt="Laughs App logo" src={`${process.env.PUBLIC_URL}/public/images/pyp-logo-cropped.png`}></img>
+                <img className="object-contain" alt="Laughs App logo" src={`${process.env.PUBLIC_URL}/pyp-logo-cropped.png`}></img>
             </div>
             <div>
                 <div className="absolute top-0 right-0 p-2 m-2">
@@ -20,7 +20,7 @@ export default function SplashPage() {
                 <h1 className="text-4xl col-start-1 col-end-2 row-start-1 row-end-2 p-1 m-1">Sharing jokes around the world</h1>
                 <h2 className="text-lg col-start-1 col-end-2 row-start-2 row-end-3 p-1 m-1">Use Laughs and all its features for free, forever!</h2>
                 <div className="col-start-2 col-end-5 row-start-1 row-end-4">
-                    <img className="rounded-lg md:w-1/2" alt="Laughing Man" src={`${process.env.PUBLIC_URL}/public/images/laughing-man-holding-pen-in-meeting.jpg`}></img>
+                    <img className="rounded-lg md:w-1/2" alt="Laughing Man" src={`${process.env.PUBLIC_URL}/laughing-man-holding-pen-in-meeting.jpg`}></img>
                 </div>
             </div>
             
