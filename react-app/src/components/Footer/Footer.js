5383cbfa05c1369617fe01ad996a2876f1adcc11
Mark Valdez <mhvaldez209@gmail.com>
2021-03-23 10:14:32 -1000

    update style on footer

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -3,14 +3,14 @@ const Footer = () => {
 
     return (
         <div className='py-1 w-full h-16 text-xl bg-gradient-to-r from-blue-joker via-red-joker to-green-joker grid grid-flow-col grid-cols-10 grid-rows-1 gap-x-3'>
-            <p>&#169; Mark Valdez</p>
-            <div className="col-start-5 col-end-5">
-                <a  className="pt-4 hover:shadow-2xl" href='https://github.com/markhv-code/pair-yo-pet'>Github Repo</a>
+            <p className="pl-2 self-center">&#169; Mark Valdez</p>
+            <div className="col-start-5 col-end-6 self-center">
+                <a  className="pt-4 hover:shadow-2xl " href='https://github.com/markhv-code/pair-yo-pet'>Github Repo</a>
             </div>
-            <div className='col-start-8 col-end-8'>
+            <div className='col-start-9 col-end-9 self-center'>
                 <a className='pt-4 hover:shadow-2xl' href='https://github.com/markhv-code'>Github Profile</a>
             </div>
-            <div className='col-start-9 col-end-9'>
+            <div className='col-start-10 col-end-10 self-center'>
                     <a className='pt-4 hover:shadow-2xl' href='https://www.linkedin.com/in/mark-valdez-a50507108/'>Linkedin</a>
             </div>
         </div>
