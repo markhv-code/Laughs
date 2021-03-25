b8aa7ed467290c295017415374c660104491cbba
Mark Valdez <mhvaldez209@gmail.com>
2021-03-25 08:18:14 -1000

    conditional activeClass

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -22,20 +22,20 @@ const NavBar = () => {
           <img className="logo" alt="Laughs App logo" src="https://pairyopet.s3-us-west-1.amazonaws.com/pyp-logo-cropped.png"></img>
         </div>
         <ul className='navbar__links'>
-          <li className='navbar__link hover:underline text-2xl'>
-            <NavLink to='/home' exact={true} activeClassName='text-3xl text-white'>
+          <li className='navbar__link'>
+            <NavLink to='/home' exact={true} className='text-2xl hover:underline ' activeClassName='text-3xl text-white hover:no-underline'>
               Home
             </NavLink>
           </li>
           {authenticated && (
             <>
-              <li className='navbar__link hover:underline text-2xl'>
-                <NavLink to='/messages' exact={true} activeClassName='text-3xl text-white'>
+              <li className='navbar__link'>
+                <NavLink to='/messages' exact={true} className='text-2xl hover:underline ' activeClassName='text-3xl text-white hover:no-underline'>
                   Messages
                 </NavLink>
               </li>
-              <li className='navbar__link hover:underline text-2xl'>
-                <NavLink to='/users' exact={true} activeClassName='text-3xl text-white'>
+              <li className='navbar__link'>
+                <NavLink to='/users' exact={true} className='text-2xl hover:underline ' activeClassName='text-3xl text-white hover:no-underline'>
                   Users
                 </NavLink>
               </li>
